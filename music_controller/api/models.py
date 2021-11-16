9cbad54d597d0a2c873c357961bdb0562cae4d12
Luca Zilio de Oliveira <lucaoliveira846@gmail.com>
2021-11-16 10:15:33 -0300

    create-room view

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -3,8 +3,7 @@ import string
 import random
 
 def generate_unique_code():
-    lenght = 6
-    
+    lenght = 6    
     while True:
         code = ''.join(random.choices(string.ascii_uppercase, k=lenght))
         if(Room.objects.filter(code=code)).count == 0:
@@ -17,7 +16,7 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default='', unique=True)
+    code = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
